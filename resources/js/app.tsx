9006c75ea7654f3e8326e950e5234131230824e1
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2026-01-29 18:59:51 +0700

    init

---
+++ resources/js/app.tsx
@@ -0,0 +1,35 @@
+import { HeroUIProvider } from '@heroui/react';
+import { createInertiaApp, router } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '../css/app.css';
+import { initializeTheme } from './hooks/use-appearance';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.tsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(
+            <StrictMode>
+                <HeroUIProvider navigate={router.visit}>
+                    <App {...props} />
+                </HeroUIProvider>
+            </StrictMode>,
+        );
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
+
+// This will set light / dark mode on load...
+initializeTheme();
