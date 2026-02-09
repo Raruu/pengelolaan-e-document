4991844a5c584c11a5d6455e85d626ff3ae52450
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2026-02-09 09:22:51 +0700

    seeder category icon

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,12 +10,12 @@ class CategorySeeder extends Seeder
     public function run()
     {
         $categories = [
-            ['category' => 'Undangan', 'direction' => 'Masuk'],
-            ['category' => 'Undangan', 'direction' => 'Keluar'],
-            ['category' => 'Telegram Biasa', 'direction' => 'Masuk'],
-            ['category' => 'Telegram Biasa', 'direction' => 'Keluar'],
-            ['category' => 'Telegram Rahasia', 'direction' => 'Masuk'],
-            ['category' => 'Telegram Rahasia', 'direction' => 'Keluar'],
+            ['category' => 'Undangan', 'direction' => 'Masuk', 'icon_path' => 'category-icon/2.webp'],
+            ['category' => 'Undangan', 'direction' => 'Keluar', 'icon_path' => 'category-icon/2.webp'],
+            ['category' => 'Telegram Biasa', 'direction' => 'Masuk', 'icon_path' => 'category-icon/4.webp'],
+            ['category' => 'Telegram Biasa', 'direction' => 'Keluar', 'icon_path' => 'category-icon/4.webp'],
+            ['category' => 'Telegram Rahasia', 'direction' => 'Masuk', 'icon_path' => 'category-icon/6.webp'],
+            ['category' => 'Telegram Rahasia', 'direction' => 'Keluar', 'icon_path' => 'category-icon/6.webp'],
         ];
 
         DB::table('categories')->insert($categories);
